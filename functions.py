651ca9bd1555ff4202bf5af33db0ec07a5d3d21a
slanuss <lanussesofi@gmail.com>
2025-05-07 15:50:00 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -104,3 +104,6 @@ def add_employee(nombre, dni, telefono, fecha_contratacion, salario):
     params = (nombre, dni, telefono, fecha_contratacion, salario)
     
     return execute_query(query, params=params, is_select=False)
+
+
+print(connect_to_supabase())
